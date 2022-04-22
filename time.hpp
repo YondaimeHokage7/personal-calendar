52cffa70d2fae7687556348b9a4d2ff961a842e4
Hristo Valev <51285393+YondaimeHokage7@users.noreply.github.com>
2022-04-22 14:36:55 +0300

    Интерфейс на клас Time

---
+++ time.hpp
@@ -0,0 +1,25 @@
+#ifndef PERSONAL_CALENDAR_TIME_HPP
+#define PERSONAL_CALENDAR_TIME_HPP
+
+//! Клас за време, който има член-данни от тип unsigned за часове и минути
+class Time
+{
+private:
+    //! Цяло число показващо часовете със стойности от 0 до 24
+    unsigned hours;
+    //! Цяло число показващо минутите със стойности от 0 до 60
+    unsigned minutes;
+public:
+    //! Конструктор с параметри
+    Time(unsigned _hours = 0, unsigned _minutes = 0);
+
+    //! Селектор за часове
+    unsigned getHours() const
+    { return hours; }
+
+    //! Селектор за часове
+    unsigned getMinutes() const
+    { return minutes; }
+};
+
+#endif //PERSONAL_CALENDAR_TIME_HPP
